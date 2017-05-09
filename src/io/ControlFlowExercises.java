102cc22fdf4aceebb71a636be0535a85fcf47730
Mike Festger <festger@gmail.com>
2017-05-09 17:23:48 -0500

    Exercise with Java
     ControlFlowExercises.java:
    
     Added:
      - Completed FizzBuzz requirements.
    
      - Began table exercise.
    
      TO DO: Complete table exercise.

--- src/io/ControlFlowExercises.java
+++ src/io/ControlFlowExercises.java
@@ -3,20 +3,38 @@
  */
 package io;
 
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         int i = 1;
-        while(i <= 100){
-            System.out.println(i);
-            i++;
-            if (i % 3 == 0){
-                System.out.println("fizz");
-            } else if (i % 5 == 0){
-                System.out.println("buzz");
-            }  if (i % 3 == 0 & i % 5 == 0){
-                System.out.println("FizzBuzz");
-            }
+
+//        System.out.println("FizzBuzz Exercise");
+//        while(i <= 100){
+//            System.out.println(i);
+//            i++;
+//            if (i % 3 == 0){
+//                System.out.println("fizz");
+//            } else if (i % 5 == 0){
+//                System.out.println("buzz");
+//            }  if (i % 3 == 0 & i % 5 == 0){
+//                System.out.println("FizzBuzz");
+//            }
+//        }
+
+        System.out.println("What number would you like to go up to? (Integers only)");
+
+        int enterInt = scan.nextInt();
+        System.out.println(enterInt);
+        int x = 1;
+        System.out.println("number");
+        while(x <= enterInt){
+            System.out.println(x);
+            x++;
         }
+
+
 
     }
 }
