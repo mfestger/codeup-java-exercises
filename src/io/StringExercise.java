f7d72f7cda5258ce882d343b9f775cd76f84b539
Mike Festger <festger@gmail.com>
2017-05-10 17:19:58 -0500

    Exercise with Java
     StringExercise.java:
    
     Added:
      - Started the first part of the exercise.
    
      TO DO:
      - Pretty much everything.

---
+++ src/io/StringExercise.java
@@ -0,0 +1,18 @@
+/**
+ * Created by michaelfestger on 5/10/17.
+ */
+package io;
+        import java.util.Scanner;
+        import java.util.Random;
+
+public class StringExercise {
+    public static void main(String[] args) {
+        String message = "We don't need no education";
+        System.out.println(message + " We don't need no thought control");
+
+
+
+
+
+    }
+}
