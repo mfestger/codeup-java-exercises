5cd1b4cb21d03900f71f57adda028247bb7b8b01
Mike Festger <festger@gmail.com>
2017-05-11 11:29:10 -0500

    Exercise with Java
     StringExercise.java:
    
     Added:
      - Completed Exercise. Very simple.

--- src/io/StringExercise.java
+++ src/io/StringExercise.java
@@ -2,15 +2,25 @@
  * Created by michaelfestger on 5/10/17.
  */
 package io;
-        import java.util.Scanner;
-        import java.util.Random;
 
 public class StringExercise {
     public static void main(String[] args) {
-        String message = "We don't need no education";
-        System.out.println(message + " We don't need no thought control");
 
+        //Exercise 1
+        String message;
+        message = "We Don't Need No Education";
+        System.out.println(message);
 
+        message = "We Don't Need No Thought Control";
+        System.out.println(message);
+
+        //Exercise 2
+        message = "Check \"this\" out";
+        System.out.println(message);
+
+        //Exercise 3 & 4
+        message = "I can do backslashes \\, couble \\\\";
+        System.out.println(message);
 
 
 
