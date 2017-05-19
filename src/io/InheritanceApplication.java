c03087635f4272bbed825ed7c357985de0be6a31
Mike Festger <festger@gmail.com>
2017-05-19 10:53:19 -0500

    Exercise with Java
    InheritanceApplication.java:
    
     Added:
      - Archived lesson.
    
      TO DO:
      - N/A

--- src/io/InheritanceApplication.java
+++ src/io/InheritanceApplication.java
@@ -5,14 +5,39 @@ package io;
  */
 public class InheritanceApplication {
     public static void main(String[] args) {
-        Person person = new Person("Mike", 29, 'M');
-        System.out.println(person.description());
-
-        Student student1 = new Student("Mike", 29, 'M', "Pinnacles", "Java");
-        System.out.println(student1.description());
-
+        // The Person class
+        Person mike = new Person("Mike", 29, 'M');
+        System.out.println(mike.description());
+        // The Person > Student class
+        // Example 1
+        Student dude = new Student("Dude", 29, 'M', "Pinnacles", "Java");
+        System.out.println(dude.description());
+        // Example 2
         Student student2 = new Student("Suzee", 32, 'F', "Pizza Hut", "Pizza");
         System.out.println(student2.description());
+
+        //student.name = "Zach"; as this is protected is not visible outside the package
+
+        Person[] people = {
+
+                new Person("Ben", 20, 'M'),
+                new Person ("Matilda", 6, 'F'),
+                new Person ("Diana Prince", 29, 'F'),
+                mike,
+                dude
+
+        };
+        for(Person person : people){
+            System.out.println(person.description());
+        }
+
+        Student[] students = {
+                new Student("Bruce Wayne", 30, 'M', "Bat Family", "Kickin' butt"),
+        };
+        for(Student student : students){
+            System.out.println(student.description());
+        }
+
     }
 
 }
