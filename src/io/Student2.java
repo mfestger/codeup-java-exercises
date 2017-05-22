91867581b9792a494dccf3563c844eeff52c93b2
Mike Festger <festger@gmail.com>
2017-05-22 14:54:50 -0500

    Exercise with Java
    Student2.java:
    
     Added:
      - Committing progress on exercise.
    
      TO DO:
      - Complete exercise.

--- src/io/Student2.java
+++ src/io/Student2.java
@@ -10,25 +10,34 @@ import java.util.ArrayList;
 public class Student2 {
 
     private String studentName;
+    int grade;
     ArrayList<Integer> grades = new ArrayList<>();
 
 
-    public Student2(String studentName) {
+    public Student2(String studentName, int grade) {
         this.studentName = studentName;
+        this.grade = grade;
     }
 
-    public String getStudentName(){
+    public String getStudentName() {
         System.out.println(studentName);
         return studentName;
     }
 
-    public void addGrade(int grade){
+    public void addGrade(int grade) {
         grades.add(grade);
     }
 
-    public double getGradeAverage(){
-        for(int grade : grades){
-            double average =+ grade;
+
+    public static void main(String[] args) {
+        Student2 mike = new Student2("Mike", 100);
+        Student2 suzee = new Student2("Suzee", 99);
+        Student2 chris = new Student2("Chris", 90);
+        Student2 lani = new Student2("Lani", 95);
+    }
+    public double getGradeAverage() {
+        for (int grade : grades) {
+            double average = +grade;
             average = average * grades.size();
             System.out.println(average);
 
@@ -36,10 +45,10 @@ public class Student2 {
         return getGradeAverage();
     }
 
-    public static void main(String[] args) {
-
-    }
-
-
-
 }
+
+
+
+
+
+
