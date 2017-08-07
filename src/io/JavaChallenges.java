5c675085172aac32ecc8acaa1b1868bab3377e21
Mike Festger <festger@gmail.com>
2017-08-07 18:06:04 -0500

    Completed Challenge 1
    Wrote instructions for challenge 2

--- src/io/JavaChallenges.java
+++ src/io/JavaChallenges.java
@@ -1,4 +1,29 @@
 package io;
 
 public class JavaChallenges {
+
+    public static void main(String[] args) {
+// Challenge 1
+//        The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.
+//        We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+
+        public boolean sleepIn ( boolean weekday, boolean vacation){
+            if (!weekday || vacation) {
+                return true;
+            }
+            return false;
+        }
+
+        //Challenge 2
+
+//        We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling.
+//                We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
+
+        public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+
+        }
+
+
+
+    }
 }
