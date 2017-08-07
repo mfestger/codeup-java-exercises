3b64ce7241988e2e9412c00c00fc0b566cfcae16
Mike Festger <festger@gmail.com>
2017-08-07 18:26:15 -0500

    Completed Challenge 6

--- src/io/JavaChallenges.java
+++ src/io/JavaChallenges.java
@@ -61,5 +61,11 @@ public class JavaChallenges {
         return (talking && (hour < 7 || hour > 20));
     }
 
+    // Challenge 6
+
+  //  Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+  public boolean makes10(int a, int b) {
+      return (a == 10 || b == 10 || a+b == 10);
+  }
 }
 }
