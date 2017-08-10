6bda9d12831bada961c8037804973203c2433d91
Mike Festger <festger@gmail.com>
2017-08-09 19:51:18 -0500

    Completed Challenge 12

--- src/io/JavaChallenges.java
+++ src/io/JavaChallenges.java
@@ -116,7 +116,6 @@ public class JavaChallenges {
 
     // Challenge 11
 
-
    // Given a string, return a new string where the first and last chars have been exchanged.
 
     public String frontBack(String str) {
@@ -128,7 +127,23 @@ public class JavaChallenges {
         return str.charAt(str.length()-1) + mid + str.charAt(0);
     }
 
+    //Challenge 12
 
+//    Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3,
+//    the front is whatever is there. Return a new string which is 3 copies of the front.
+
+    public String front3(String str) {
+        String front;
+
+        if (str.length() >= 3) {
+            front = str.substring(0, 3);
+        }
+        else {
+            front = str;
+        }
+
+        return front + front + front;
+    }
 
 }
 }
