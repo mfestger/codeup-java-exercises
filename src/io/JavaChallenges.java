c2368d5f4fa77321188066d42dca731a8f981053
Mike Festger <festger@gmail.com>
2017-08-09 20:00:20 -0500

    Completed Challenge 14

--- src/io/JavaChallenges.java
+++ src/io/JavaChallenges.java
@@ -155,5 +155,16 @@ public class JavaChallenges {
         return myString + str + myString;
     }
 
+    // Challenge 14
+
+//    Return true if the given non-negative number is a multiple of 3 or a multiple of 5.
+//    Use the % "mod" operator -- see Introduction to Mod
+
+    public boolean or35(int n) {
+        return (n % 5 == 0) || (n % 3 == 0);
+    }
+
+
+
 }
 }
