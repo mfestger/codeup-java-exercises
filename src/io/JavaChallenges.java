f58d82425b8ef9c0a1cb8e250334ccc9532fd71b
Mike Festger <festger@gmail.com>
2017-08-10 17:32:43 -0500

    Completed Challenge 15

--- src/io/JavaChallenges.java
+++ src/io/JavaChallenges.java
@@ -164,6 +164,20 @@ public class JavaChallenges {
         return (n % 5 == 0) || (n % 3 == 0);
     }
 
+// Challenge 15
+
+//    Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back,
+//    so "kitten" yields"kikittenki". If the string length is less than 2, use whatever chars are there.
+
+    public String front22(String str) {
+        int take = 2;
+        if (take > str.length()) {
+            take = str.length();
+        }
+
+        String front = str.substring(0, take);
+        return front + str + front;
+    }
 
 
 }
