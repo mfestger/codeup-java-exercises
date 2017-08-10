412d4b542dfcaaf9405178a48c1bfbcfebca48e9
Mike Festger <festger@gmail.com>
2017-08-09 19:40:58 -0500

    Completed Challenge 11

--- src/io/JavaChallenges.java
+++ src/io/JavaChallenges.java
@@ -113,5 +113,22 @@ public class JavaChallenges {
 
         return front + back;
     }
+
+    // Challenge 11
+
+
+   // Given a string, return a new string where the first and last chars have been exchanged.
+
+    public String frontBack(String str) {
+        if (str.length() <= 1) return str;
+
+        String mid = str.substring(1, str.length()-1);
+
+        // last + mid + first
+        return str.charAt(str.length()-1) + mid + str.charAt(0);
+    }
+
+
+
 }
 }
