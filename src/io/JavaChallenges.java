8f3fdaa7b7f4bb9ac5fb2621fd75ed3d4dc8f6f9
Mike Festger <festger@gmail.com>
2017-08-12 10:28:51 -0500

    Completed Challenge 20

--- src/io/JavaChallenges.java
+++ src/io/JavaChallenges.java
@@ -220,6 +220,18 @@ public class JavaChallenges {
                 (c>=13 && c<=19);
     }
 
+    // Challenge 20
+
+
+//    Given a string, if the string "del" appears starting at index 1, return a string where that "del" has been deleted.
+//            Otherwise, return the string unchanged.
+
+    public String delDel(String str) {
+        if (str.length()>=4 && str.substring(1, 4).equals("del")) {
+            return str.substring(0, 1) + str.substring(4);
+        }
+        return str;
+    }
 
 
 }
