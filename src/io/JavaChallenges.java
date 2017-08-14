742f5baf1fd5a2fe172275d928dac9ef669d5a6f
Mike Festger <festger@gmail.com>
2017-08-14 16:58:54 -0500

    Completed Challenge 21

--- src/io/JavaChallenges.java
+++ src/io/JavaChallenges.java
@@ -233,6 +233,23 @@ public class JavaChallenges {
         return str;
     }
 
+    // Challenge 21
+
+//    Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix" ..
+//    all count.Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix" .. all count.
+public boolean mixStart(String str) {
+    if (str.length() < 3) return false;
+
+
+    String two = str.substring(1, 3);
+
+    if (two.equals("ix")) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
 
 }
 }
