8a15ba36cf9323deccd4290b9887db728a633a0a
Mike Festger <festger@gmail.com>
2017-08-16 18:33:08 -0500

    Completed Challenge 25

--- src/io/JavaChallenges.java
+++ src/io/JavaChallenges.java
@@ -312,6 +312,20 @@ public boolean mixStart(String str) {
         return 0;  // i.e. aDiff == bDiff
     }
 
+    // Challenge 25
+
+
+   // Given 2 int values, return true if they are both in the range 30..40 inclusive, or they are both in the range 40..50 inclusive.
+
+    public boolean in3050(int a, int b) {
+        if (a >= 30 && a <= 40 && b >= 30 && b <= 40) {
+            return true;
+        }
+        if (a >= 40 && a <= 50 && b >= 40 && b <= 50) {
+            return true;
+        }
+        return false;
+    }
 
 }
 }
