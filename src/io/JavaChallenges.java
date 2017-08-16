1b8e3e215ca24bdca7834a0c372dd238d2b69855
Mike Festger <festger@gmail.com>
2017-08-16 18:30:13 -0500

    Completed Challenge 24

--- src/io/JavaChallenges.java
+++ src/io/JavaChallenges.java
@@ -293,6 +293,25 @@ public boolean mixStart(String str) {
 
     }
 
+    // Challenge 24
+
+
+//    Given 2 int values, return whichever value is nearest to the value 10, or return 0 in the event of a tie.
+//    Note that Math.abs(n) returns the absolute value of a number.
+
+    public int close10(int a, int b) {
+        int aDiff = Math.abs(a - 10);
+        int bDiff = Math.abs(b - 10);
+
+        if (aDiff < bDiff) {
+            return a;
+        }
+        if (bDiff < aDiff) {
+            return b;
+        }
+        return 0;  // i.e. aDiff == bDiff
+    }
+
 
 }
 }
