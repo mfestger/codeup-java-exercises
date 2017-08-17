4fcc5e25377bd8a2f88dd65e094057165a3c7e3c
Mike Festger <festger@gmail.com>
2017-08-17 18:05:20 -0500

    Completed Challenge 31

--- src/io/JavaChallenges.java
+++ src/io/JavaChallenges.java
@@ -399,6 +399,20 @@ public class JavaChallenges {
         }
     }
 
+    // Medium Difficulty
+
+    // Challenge 31
+
+    // Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+    public String stringTimes(String str, int n) {
+        String result = "";
+        for (int i=0; i<n; i++) {
+            result = result + str;  // could use += here
+        }
+        return result;
+    }
+
 
 }
 }
