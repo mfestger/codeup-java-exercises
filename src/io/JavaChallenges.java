ecdba6a0c060f27b428acbc559dea5a45d583eef
Mike Festger <festger@gmail.com>
2017-08-16 19:45:16 -0500

    Completed Challenge 30

--- src/io/JavaChallenges.java
+++ src/io/JavaChallenges.java
@@ -382,5 +382,23 @@ public class JavaChallenges {
         return front + back.toUpperCase();
     }
 
+    // Challenge 30
+
+
+//    Given a non-empty string and an int N, return the string made starting with char 0, and then
+//    every Nth char of the string. So if N is 3, use char 0, 3, 6, ... and so on. N is 1 or more.
+
+    public String everyNth(String str, int n) {
+        public String everyNth(String str, int n) {
+            String result = "";
+
+            for (int i=0; i<str.length(); i = i + n) {
+                result = result + str.charAt(i);
+            }
+            return result;
+        }
+    }
+
+
 }
 }
