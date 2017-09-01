2f2669ad86e61b77678415994d2768c8ce7b15d1
Mike Festger <festger@gmail.com>
2017-08-31 19:34:23 -0500

    Completed Challenge 35

--- src/io/JavaChallenges.java
+++ src/io/JavaChallenges.java
@@ -463,7 +463,19 @@ public class JavaChallenges {
         // return x.startsWith("xx");
     }
 
+    // Challenge 35
 
+   // Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+
+    public String stringBits(String str) {
+        String result = "";
+        // Note: the loop increments i by 2
+        for (int i=0; i<str.length(); i+=2) {
+            result = result + str.substring(i, i+1);
+            // Alternately could use str.charAt(i)
+        }
+        return result;
+    }
 
 
 
