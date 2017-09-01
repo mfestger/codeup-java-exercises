620643e657a8b1b3e158f7ece8a9e0f23a3cc4c4
Mike Festger <festger@gmail.com>
2017-08-31 19:28:21 -0500

    Completed Challenge 34

--- src/io/JavaChallenges.java
+++ src/io/JavaChallenges.java
@@ -445,5 +445,27 @@ public class JavaChallenges {
         }
         return count;
     }
+
+    // Challenge 34
+
+    //Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
+
+    boolean doubleX(String str) {
+        int i = str.indexOf("x");
+        if (i == -1) return false; // no "x" at all
+
+        // Is char at i+1 also an "x"?
+        if (i+1 >= str.length()) return false; // check i+1 in bounds?
+        return str.substring(i+1, i+2).equals("x");
+
+        // Another approach -- .startsWith() simplifies the logic
+        // String x = str.substring(i);
+        // return x.startsWith("xx");
+    }
+
+
+
+
+
 }
 }
